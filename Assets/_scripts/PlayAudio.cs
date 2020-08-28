8a3f404204c48f4e473fe201e05490c9bedcfb99
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2020-08-28 03:42:01 +0200

    Add files via upload

---
+++ Assets/_scripts/PlayAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+	public GvrAudioSource background;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayAudioSource(GvrAudioSource stationAudio){
+		//gvrAudio.Play ();
+		if (stationAudio.isPlaying) {
+			stationAudio.Pause ();
+			background.Play ();
+		} else {
+			stationAudio.Play ();
+			background.Pause ();
+		}
+	}
+}
